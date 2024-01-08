794069895f66d9fc0498320230e133b8166142c9
Hash <halchemist@tutamail.com>
2024-01-08 23:22:25 +0400

    refactor: Change names of parameters for function `division`
    Changes the name from number1 and number2 to numerator and denominator

--- script.js
+++ script.js
@@ -10,6 +10,6 @@ function multiplication(number1, number2){
     return number1 * number2
 }
 
-function division(number1, number2){
+function division(numerator, denominator){
     return number2==0? "infinity" : number1/number2;
 }
