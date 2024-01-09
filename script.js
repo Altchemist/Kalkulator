2c0734461175079ff4d7b37d919021d3c74d3221
Hash <halchemist@tutamail.com>
2024-01-09 23:15:57 +0400

    feat: Implement function `operate` to handle operator and numbers

--- script.js
+++ script.js
@@ -14,4 +14,26 @@ function division(numerator, denominator){
     return denominator==0? "infinity" : numerator/denominator;
 }
 
-module.exports = {addition, substraction, multiplication, division}; 
+function infixToPostfix(expression){
+    // try to understand the infix and prefix idea by myself
+}
+
+function operate(operator, number1, number2){
+    switch(operator){
+        case "+":
+            return addition(number1, number2);
+        case "-":
+            return substraction(number1, number2);
+        case "*":
+            return multiplication(number1, number2);
+        case "/":
+            return division(number1, number2)
+    };
+};
+
+let op = "";
+
+let num1 = 0;
+let num2 = 0;
+
+module.exports = {addition, substraction, multiplication, division, infixToPostfix, operate}; 
